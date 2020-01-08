b02a4f29e2bc04beae3e71f6c0ad66bb99a3ea38
edvijura <edvinas.juraska@lzukt.lt>
2020-01-08 11:06:03 +0200

    Tutorial.

--- app/src/main/java/edvinasnew/app/TutorialActivity.kt
+++ app/src/main/java/edvinasnew/app/TutorialActivity.kt
@@ -1,15 +1,17 @@
 package edvinasnew.app
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class TutorialActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startActivity(Intent(this, TutorialActivity::class.java))
+        setContentView(R.layout.activity_tutorial)
+        findViewById<Button>(R.id.next).setOnClickListener {
+                finish()
+        }
+
     }
 }
