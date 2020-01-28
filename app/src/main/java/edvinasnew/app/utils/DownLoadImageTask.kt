e98471a6d1ea2e0bf6bbd0adfed9ee6e63ce4e8d
edvijura <edvinas.juraska@lzukt.lt>
2020-01-28 16:03:05 +0200

    Lint fix

--- app/src/main/java/edvinasnew/app/utils/DownLoadImageTask.kt
+++ app/src/main/java/edvinasnew/app/utils/DownLoadImageTask.kt
@@ -9,26 +9,26 @@ import android.widget.Toast
 import java.io.IOException
 import java.net.URL
 
-class DownLoadImageTask(internal val imageView: ImageView) :
-    AsyncTask<String, Void, Bitmap?>() {
-    override fun doInBackground(vararg urls: String): Bitmap? {
-        val urlOfImage = urls[0]
-        return try {
-            val inputStream = URL(urlOfImage).openStream()
-            BitmapFactory.decodeStream(inputStream)
-        } catch (e: IOException) {
-            Log.d("error", e.message.orEmpty())
-            // e.printStackTrace()
-            null
-        }
-    }
-
-    override fun onPostExecute(result: Bitmap?) {
-        if (result != null) {
-            Toast.makeText(imageView.context, "download success", Toast.LENGTH_SHORT).show()
-            imageView.setImageBitmap(result)
-        } else {
-            Toast.makeText(imageView.context, "Error downloading", Toast.LENGTH_SHORT).show()
-        }
-    }
-}
+//class DownLoadImageTask(internal val imageView: ImageView) :
+//    AsyncTask<String, Void, Bitmap?>() {
+//    override fun doInBackground(vararg urls: String): Bitmap? {
+//        val urlOfImage = urls[0]
+//        return try {
+//            val inputStream = URL(urlOfImage).openStream()
+//            BitmapFactory.decodeStream(inputStream)
+//        } catch (e: IOException) {
+//            Log.d("error", e.message.orEmpty())
+//            // e.printStackTrace()
+//            null
+//        }
+//    }
+//
+//    override fun onPostExecute(result: Bitmap?) {
+//        if (result != null) {
+//            Toast.makeText(imageView.context, "download success", Toast.LENGTH_SHORT).show()
+//            imageView.setImageBitmap(result)
+//        } else {
+//            Toast.makeText(imageView.context, "Error downloading", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//}
